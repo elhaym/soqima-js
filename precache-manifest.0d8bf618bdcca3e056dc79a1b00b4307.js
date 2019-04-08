ef6993cd065de4066d74acf81671c8442fbd9e2a
Elhaym <elhaym@users.noreply.github.com>
2019-04-08 03:31:25 +0200

    Updates

---
+++ precache-manifest.0d8bf618bdcca3e056dc79a1b00b4307.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dc69c3f577e3fd21c0d",
+    "url": "/soqima-js/static/css/main.597de5c6.chunk.css"
+  },
+  {
+    "revision": "2dc69c3f577e3fd21c0d",
+    "url": "/soqima-js/static/js/main.07ad1afa.chunk.js"
+  },
+  {
+    "revision": "8f6d01554ca01a3864c3",
+    "url": "/soqima-js/static/js/runtime~main.9eeacc61.js"
+  },
+  {
+    "revision": "df0ae5558613e95144b7",
+    "url": "/soqima-js/static/js/2.560dde89.chunk.js"
+  },
+  {
+    "revision": "5f25f98ad449e14f35dbe987e0d090e8",
+    "url": "/soqima-js/static/media/manrope-extrabold.5f25f98a.woff"
+  },
+  {
+    "revision": "a6fa3bd125ff6df3f77ce6c4203e4282",
+    "url": "/soqima-js/static/media/manrope-bold.a6fa3bd1.woff2"
+  },
+  {
+    "revision": "7af88092dbd37f08241da9f416dee798",
+    "url": "/soqima-js/static/media/manrope-extrabold.7af88092.woff2"
+  },
+  {
+    "revision": "8325e7ea6f0adcae48d1dbc6e45d1951",
+    "url": "/soqima-js/static/media/manrope-semibold.8325e7ea.woff"
+  },
+  {
+    "revision": "a00289fe0098f0dc10fcc3ece8073bb6",
+    "url": "/soqima-js/static/media/manrope-semibold.a00289fe.woff2"
+  },
+  {
+    "revision": "441244cf44090a0ff88abc33fbbca97d",
+    "url": "/soqima-js/static/media/manrope-medium.441244cf.woff2"
+  },
+  {
+    "revision": "3c100c613625f06a5e00ddbba18b7f61",
+    "url": "/soqima-js/static/media/manrope-regular.3c100c61.woff2"
+  },
+  {
+    "revision": "3cea6e29c4001d672667a93775cb061e",
+    "url": "/soqima-js/static/media/manrope-regular.3cea6e29.woff"
+  },
+  {
+    "revision": "37c00b97e26ac9fe903a6ccc9ea48644",
+    "url": "/soqima-js/static/media/manrope-light.37c00b97.woff2"
+  },
+  {
+    "revision": "6c9f93a2edff065d81710586bf74fbe3",
+    "url": "/soqima-js/static/media/manrope-thin.6c9f93a2.woff2"
+  },
+  {
+    "revision": "c246dc6aecd6b7007b935466d04e3082",
+    "url": "/soqima-js/static/media/manrope-bold.c246dc6a.woff"
+  },
+  {
+    "revision": "16a2e274512a3e5f752ee83b66ed0d9d",
+    "url": "/soqima-js/static/media/manrope-medium.16a2e274.woff"
+  },
+  {
+    "revision": "484b6c5b3a6f889b62b1985d44467096",
+    "url": "/soqima-js/static/media/manrope-thin.484b6c5b.woff"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "/soqima-js/static/media/MaterialIcons-Regular.bca3a187.woff2"
+  },
+  {
+    "revision": "860d372f9b96d1b53a5b82a6cec33878",
+    "url": "/soqima-js/static/media/manrope-light.860d372f.woff"
+  },
+  {
+    "revision": "b661c28b0f28606a96722ad2d9588b70",
+    "url": "/soqima-js/static/media/MaterialIcons-Regular.b661c28b.eot"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "/soqima-js/static/media/MaterialIcons-Regular.9219a80f.woff"
+  },
+  {
+    "revision": "6b7897c9d28b17fce51b4b20052bf93f",
+    "url": "/soqima-js/static/media/manrope-extrabold.6b7897c9.otf"
+  },
+  {
+    "revision": "cb519ea75f9f92a1ed78efdf2f4fdfef",
+    "url": "/soqima-js/static/media/manrope-bold.cb519ea7.otf"
+  },
+  {
+    "revision": "ae7c5af0c35d94dbcd1c599fc7b400b7",
+    "url": "/soqima-js/static/media/manrope-semibold.ae7c5af0.otf"
+  },
+  {
+    "revision": "31973fa52e481ba490f242d2ab896fd2",
+    "url": "/soqima-js/static/media/manrope-medium.31973fa5.otf"
+  },
+  {
+    "revision": "b0a4a6ccf7d97d473f4590541a20f950",
+    "url": "/soqima-js/static/media/manrope-regular.b0a4a6cc.otf"
+  },
+  {
+    "revision": "85b96796bdaef0c326c1bbb18f0222ea",
+    "url": "/soqima-js/static/media/manrope-light.85b96796.otf"
+  },
+  {
+    "revision": "ab2fd20ee1acaabb676ee6fd2432437b",
+    "url": "/soqima-js/static/media/manrope-thin.ab2fd20e.otf"
+  },
+  {
+    "revision": "684054b30da2d63ba68f62a3d51be59c",
+    "url": "/soqima-js/static/media/bg_map_800x800.684054b3.jpg"
+  },
+  {
+    "revision": "586090b38a233ce0201fb221eb117a36",
+    "url": "/soqima-js/static/media/MaterialIcons-Regular.586090b3.ttf"
+  },
+  {
+    "revision": "da4cda2470c7d54795d49df0070c0be6",
+    "url": "/soqima-js/index.html"
+  }
+];
